ba4d317d8d06fb97a2521ad702defa7bd35f4dd6
maxtes <maksim.teslenko@gmail.com>
2024-03-19 13:59:23 +0100

    Cleanup and new components

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,3 +1,10 @@
+import { NoteForm } from "./NoteForm";
+
 export function NewNote () {
-	return <h1>New</h1>
+	return (
+		<>
+			<h1 className="mb-4">New Note</h1>
+			<NoteForm />
+		</>
+	)
 } 	
