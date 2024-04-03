8a55b6fb3be4669a1443bf2caed22b6ec04ec848
maxtes <maksim.teslenko@gmail.com>
2024-04-03 11:45:31 +0200

    Note list page final

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,21 +1,34 @@
 import { useMemo, useState } from "react";
-import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
-import { Note, Tag } from "./App";
+import { Tag } from "./App";
+import styles from "./NoteList.module.css"
+
+type SimplifiedNote = {
+	tags: Tag[];
+	title: string;
+	id: string;
+}
 
 type NoteListProps = {
 	availableTags: Tag[];
-	notes: Note[];
+	notes: SimplifiedNote[];
 }
 
 export function NoteList ({availableTags, notes} : NoteListProps) {
 	const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-	const [title, setTitle] = useState("string");
+	const [title, setTitle] = useState("");
 
 	const filteredNotes = useMemo(()=> {
 		return notes.filter(note => {
-			return (title === "" || note.title.toLowerCase().includes(title.toLowerCase())) && (selectedTags.length === 0 || selectedTags.every(tag => note.tags.some))
+			return (
+				title === "" || 
+					note.title.toLowerCase().includes(title.toLowerCase())) && 
+				(selectedTags.length === 0 || 
+					selectedTags.every(tag =>
+						note.tags.some(noteTag => noteTag.id === tag.id)
+					))
 		})
 	}, [title, selectedTags, notes]);
 
@@ -62,12 +75,29 @@ export function NoteList ({availableTags, notes} : NoteListProps) {
 				</Row>
 			</Form>
 			<Row xs={1} sm={2} lg={3} xl={4} className="g-3">
-				{filteredNotes.map(note => {
+				{filteredNotes.map(note => (
 					<Col key={note.id}>
-						<NoteCard />
+						<NoteCard id={note.id} title={note.title} tags={note.tags} />
 					</Col>
-				})}
+				))}
 			</Row>
 		</>
 	)
 } 	
+
+function NoteCard({id, title, tags}: SimplifiedNote) {
+	return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+		<Card.Body>
+			<Stack gap={2} className="align-items-center justify-content-center h-100">
+				<span className="fs-5">{title}</span>
+				{tags.length > 0 && (
+					<Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+						{tags.map(tag => (
+							<Badge className="text-truncate" key={tag.id}>{tag.label}</Badge>
+						))}
+					</Stack>
+				)}
+			</Stack>
+		</Card.Body>
+	</Card>
+}
